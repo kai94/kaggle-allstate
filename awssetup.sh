6edfacce1de7f9b142eed8ad13f5779af84260eb
Ubuntu <ubuntu@ip-172-31-21-2.ec2.internal>
2016-12-06 14:16:58 +0000

    added awscli

--- awssetup.sh
+++ awssetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python-numpy python-pandas emacs24 python-pip -y
+sudo apt-get install python-numpy python-pandas emacs24 python-pip awscli -y
 
 export LC_ALL=C
 pip install sklearn xgboost
